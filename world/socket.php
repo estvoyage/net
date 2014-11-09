ace17ef27a2a6a2d5d2ac364799bf80a5680cb56
Frédéric Hardy <frederic.hardy@mageekbox.net>
2014-11-09 20:59:03 +0100

    Remove useless methods.

--- world/socket.php
+++ world/socket.php
@@ -4,8 +4,6 @@ namespace estvoyage\net\world;
 
 interface socket extends endpoint
 {
-	function connectHost($host);
-	function connectPort($port);
 	function writeData($data, callable $dataRemaining);
 	function write(socket\data $data);
 	function shutdown();
