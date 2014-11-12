e03a17802eb5e4583787f6b4e564f1f841da71bf
Frédéric Hardy <frederic.hardy@mageekbox.net>
2014-11-12 22:57:21 +0100

    Add UDP connection.

---
+++ classes/connection/udp.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace estvoyage\net\connection;
+
+use
+	estvoyage\net\world as net,
+	estvoyage\net\connection,
+	estvoyage\net\endpoint\socket
+;
+
+class udp extends connection
+{
+	function __construct(net\endpoint\address $address)
+	{
+		parent::__construct($address, new socket\protocol\udp);
+	}
+}
---
+++ tests/units/classes/connection/udp.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace estvoyage\net\tests\units\connection;
+
+require __DIR__ . '/../../Runner.php';
+
+use
+	estvoyage\net\tests\units,
+	estvoyage\net\endpoint\socket,
+	mock\estvoyage\net\world as net
+;
+
+class udp extends units\test
+{
+	function testClass()
+	{
+		$this->testedClass
+			->implements('estvoyage\net\world\connection')
+		;
+	}
+
+	function test__construct()
+	{
+		$this
+			->given(
+				$this->calling($address = new net\endpoint\address)->connect = function($endpoint, $callback) { $callback(new net\endpoint\socket); }
+			)
+			->if(
+				$this->newTestedInstance($address)
+			)
+			->then
+				->mock($address)->call('connect')->withArguments(new socket(new socket\protocol\udp))->once
+		;
+	}
+}
