85ebd181a9e1ae7796383c80bd0e21cec99e7cb7
Frédéric Hardy <frederic.hardy@mageekbox.net>
2014-12-31 13:24:00 +0100

    Add host::validate().

--- classes/host.php
+++ classes/host.php
@@ -3,9 +3,27 @@
 namespace estvoyage\net;
 
 use
-	estvoyage\value
+	estvoyage\value\world as value
 ;
 
-final class host extends value\string
+final class host
 {
+	use value\string {
+		validate as isString;
+	}
+
+	static function validate($value)
+	{
+		switch (true)
+		{
+			case ! self::isString($value):
+			case $value === '':
+			case ! preg_match('#^(?:[a-z0-9][-a-z0-9]{0,62}\.)*[a-z0-9][-a-z0-9]{0,62}(?<!-)\.?$#i', $value):
+			case strlen(rtrim($value, '.')) > 253:
+				return false;
+
+			default:
+				return true;
+		}
+	}
 }
--- tests/units/classes/host.php
+++ tests/units/classes/host.php
@@ -4,13 +4,34 @@ namespace estvoyage\net\tests\units;
 
 require __DIR__ . '/../runner.php';
 
+use
+	estvoyage\net\host as testedClass
+;
+
 class host extends test
 {
 	function testClass()
 	{
 		$this->testedClass
-			->extends('estvoyage\value\string')
+			->isFinal
 		;
+	}
+
+	/**
+	 * @dataProvider validValueProvider
+	 */
+	function testValidateWithValidValue($value)
+	{
+		$this->boolean(testedClass::validate($value))->isTrue;
+	}
+
+
+	/**
+	 * @dataProvider invalidValueProvider
+	 */
+	function testValidateWithInvalidValue($value)
+	{
+		$this->boolean(testedClass::validate($value))->isFalse;
 	}
 
 	function testProperties()
@@ -48,5 +69,36 @@ class host extends test
 					->isInstanceOf('logicException')
 					->hasMessage(get_class($this->testedInstance) . ' is immutable')
 		;
+	}
+
+	protected function validValueProvider()
+	{
+		return [
+			'single letter' => 'a',
+			'single letter with trainling dot' => 'a.',
+			'single digit' => '1',
+			'single digit with trailing dot' => '1.',
+			'single letter, dot, single digit' => 'a.1',
+			'63 letters, dot, 63 digits' => str_repeat('a', 63) . '.' . str_repeat('1', 63)
+		];
+	}
+
+	protected function invalidValueProvider()
+	{
+		return [
+			'null' => null,
+			'true '=> true,
+			'false' => false,
+			'any integer' => rand(- PHP_INT_MAX, PHP_INT_MAX),
+			'any float' => (float) rand(-PHP_INT_MAX, PHP_INT_MAX),
+			'an array' => [ [] ],
+			'an object' => new \stdclass,
+			'empty string' => '',
+			'string with more than 254 characters' => str_repeat('a', 255),
+			'string with more than 63 characters without dot' => str_repeat('a', 64),
+			'single hyphen' => '-',
+			'string with trailing hyphen' => str_repeat('a', rand(1, 62)) . '-',
+			'string which start with hyphen' => '-' . str_repeat('a', rand(1, 62))
+		];
 	}
 }
