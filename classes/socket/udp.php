47c2d2c880aa3f557434bbdc62403d1371095854
Frédéric Hardy <frederic.hardy@mageekbox.net>
2015-01-16 22:43:43 +0100

    Add block/non block message on socket.

--- classes/socket/udp.php
+++ classes/socket/udp.php
@@ -37,20 +37,50 @@ class udp implements net\socket
 		$this->resource = null;
 	}
 
+	function bufferWantToWait()
+	{
+		return $this->block(true);
+	}
+
+	function bufferDoNotWantToWait()
+	{
+		return $this->block(false);
+	}
+
 	function bufferContains(net\socket\buffer $buffer, data $data)
 	{
 		$dataLength = strlen($data);
 
-		switch (true)
+		if (($bytesWritten = socket_sendto($this->connectToHost()->resource, $data, $dataLength, 0, $this->host, $this->port)) === false)
 		{
-			case ! $this->resource && ! ($this->resource = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP) ?: null):
-			case ($bytesWritten = socket_sendto($this->resource, $data, $dataLength, 0, $this->host, $this->port)) === false:
-				throw new exception(new error(new error\code(socket_last_error($this->resource))));
+			throw new exception(new error(new error\code(socket_last_error($this->resource))));
 		}
 
 		if ($bytesWritten < $dataLength)
 		{
 			$buffer->remainingData(new data(substr($data, $bytesWritten)));
+		}
+
+		return $this;
+	}
+
+	private function connectToHost()
+	{
+		if (! $this->resource && ! ($this->resource = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP) ?: null))
+		{
+			throw new exception(new error(new error\code(socket_last_error($this->resource))));
+		}
+
+		return $this;
+	}
+
+	private function block($boolean)
+	{
+		$this->connectToHost();
+
+		if (! ($boolean ? socket_set_block($this->resource) : socket_set_nonblock($this->resource)))
+		{
+			throw new exception(new error(new error\code(socket_last_error($this->resource))));
 		}
 
 		return $this;
