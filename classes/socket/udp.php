60541e466dcb78cb4cf6f09776d49b455750f327
Frédéric Hardy <frederic.hardy@mageekbox.net>
2015-01-21 18:51:54 +0100

    Remove useless methods.

--- classes/socket/udp.php
+++ classes/socket/udp.php
@@ -40,16 +40,6 @@ class udp implements net\socket
 		}
 	}
 
-	function bufferWantToWait()
-	{
-		return $this->block(true);
-	}
-
-	function bufferDoNotWantToWait()
-	{
-		return $this->block(false);
-	}
-
 	function bufferContains(net\socket\buffer $buffer, data $data)
 	{
 		$dataLength = strlen($data);
@@ -70,18 +60,6 @@ class udp implements net\socket
 	private function connectToHost()
 	{
 		if (! $this->resource && ! ($this->resource = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP) ?: null))
-		{
-			throw new exception(new error(new error\code(socket_last_error($this->resource))));
-		}
-
-		return $this;
-	}
-
-	private function block($boolean)
-	{
-		$this->connectToHost();
-
-		if (! ($boolean ? socket_set_block($this->resource) : socket_set_nonblock($this->resource)))
 		{
 			throw new exception(new error(new error\code(socket_last_error($this->resource))));
 		}
