4a30f98b6a61ba2cf779523fc9ebcc2cfd42a915
Frédéric Hardy <frederic.hardy@mageekbox.net>
2015-03-02 21:46:12 +0100

    Use delimiter in mtu.

--- tests/units/classes/mtu.php
+++ tests/units/classes/mtu.php
@@ -5,7 +5,9 @@ namespace estvoyage\net\tests\units;
 require __DIR__ . '/../runner.php';
 
 use
-	estvoyage\net\mtu as testedClass
+	estvoyage\net\mtu as testedClass,
+	estvoyage\data,
+	mock\estvoyage\data as mockedData
 ;
 
 class mtu extends test
@@ -14,107 +16,61 @@ class mtu extends test
 	{
 		$this->testedClass
 			->extends('estvoyage\value\integer\unsigned')
+			->implements('estvoyage\data\splitter')
 			->isFinal
 		;
 	}
 
-	/**
-	 * @dataProvider validValueProvider
-	 */
-	function testConstructordWithValidValue($value)
-	{
-		$this->integer($this->newTestedInstance($value)->asInteger)->isEqualTo($value);
-	}
-
-	/**
-	 * @dataProvider invalidValueProvider
-	 */
-	function testConstructorWithInvalidValue($value)
-	{
-		$this->exception(function() use ($value) { $this->newTestedInstance($value); })
-			->isInstanceOf('domainException')
-			->hasMessage('Argument should be an integer greater than or equal to 68')
-		;
-	}
-
-	/**
-	 * @dataProvider validValueProvider
-	 */
-	function testValidateWithValidValue($value)
-	{
-		$this->boolean(testedClass::validate($value))->isTrue;
-	}
-
-	/**
-	 * @dataProvider invalidValueProvider
-	 */
-	function testValidateWithInvalidValue($value)
-	{
-		$this->boolean(testedClass::validate($value))->isFalse;
-	}
-
-	function testProperties()
+	function testDataForDataConsumerUseDelimiter()
 	{
 		$this
 			->given(
-				$value = rand(68, PHP_INT_MAX)
+				$dataConsumer = new mockedData\consumer,
+				$delimiter = new mockedData\data\delimiter,
+				$this->newTestedInstance(68)
 			)
 			->if(
-				$this->newTestedInstance($value)
+				$data = new data\data(str_repeat('0', 68))
 			)
 			->then
-				->integer($this->testedInstance->asInteger)->isIdenticalTo($value)
+				->object($this->testedInstance->dataForDataConsumerUseDataDelimiter($data, $dataConsumer, $delimiter))->isTestedInstance
+				->mock($dataConsumer)
+					->receive('newData')
+						->withArguments($data)->once
 
-				->exception(function() use (& $property) { $this->testedInstance->{$property = uniqid()}; })
-					->isInstanceOf('logicException')
-					->hasMessage('Undefined property: ' . get_class($this->testedInstance) . '::' . $property)
-
-				->boolean(isset($this->testedInstance->asInteger))->isTrue
-				->boolean(isset($this->testedInstance->{uniqid()}))->isFalse
-		;
-	}
-
-	function testImmutability()
-	{
-		$this
-			->given(
-				$value = rand(68, PHP_INT_MAX)
-			)
 			->if(
-				$this->newTestedInstance($value)
+				$data = new data\data(str_repeat('1', 69)),
+				$this->calling($delimiter)->dataSplitterNeedDelimitedDataFromData[1] = function($dataSplitter, $data) {
+					$dataSplitter->noDelimiterInData($data);
+				}
 			)
 			->then
-				->exception(function() { $this->testedInstance->{uniqid()} = uniqid(); })
-					->isInstanceOf('logicException')
-					->hasMessage(get_class($this->testedInstance) . ' is immutable')
+				->exception(function()
+						use (
+							$data,
+							$dataConsumer,
+							$delimiter
+						) {
+						$this->testedInstance->dataForDataConsumerUseDataDelimiter($data, $dataConsumer, $delimiter);
+					}
+				)
+					->isInstanceOf('estvoyage\net\mtu\overflow')
+					->hasMessage('Unable to split data according to MTU 68')
 
-				->exception(function() { unset($this->testedInstance->{uniqid()}); })
-					->isInstanceOf('logicException')
-					->hasMessage(get_class($this->testedInstance) . ' is immutable')
+			->if(
+				$data = new data\data(str_repeat('2', 68) . '3'),
+
+				$this->calling($delimiter)->dataSplitterNeedDelimitedDataFromData[2] = function($dataSplitter, $data) {
+					$dataSplitter->delimitedDataIs(new data\data(str_repeat('2', 68)));
+				},
+
+				$this->testedInstance->dataForDataConsumerUseDataDelimiter($data, $dataConsumer, $delimiter)
+			)
+			->then
+				->mock($dataConsumer)
+					->receive('newData')
+						->withArguments(new data\data(str_repeat('2', 68)))->once
+						->withArguments(new data\data('3'))->once
 		;
-	}
-
-	protected function validValueProvider()
-	{
-		return [
-			'any integer greater than or equal to 68' => rand(68, PHP_INT_MAX)
-		];
-	}
-
-	protected function invalidValueProvider()
-	{
-		return [
-			'null' => null,
-			'true' => true,
-			'false' => false,
-			'empty string' => '',
-			'any string' => uniqid() . ' ' . uniqid(),
-			'any negative integer' => - rand(1, PHP_INT_MAX),
-			'zero as integer',
-			'any integer between 1 and 67' => rand(1, 67),
-			'any integer as float' => (float) rand(-PHP_INT_MAX, PHP_INT_MAX),
-			'array' => [ [] ],
-			'object' => new \stdclass
-		];
 	}
 }
